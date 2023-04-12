a0b089c111b89d4b8a2bcde1f1ecc66dad044148
Ilias Moreno <iliasete@gmail.com>
2023-04-12 14:48:46 +0200

    add navigation and lazyloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'pageA',
+    loadComponent: () => import('./pages/page-a/page-a.component').then(m => m.PageAComponent)
+  },
+  {
+    path: 'pageB',
+    loadComponent: () => import('./pages/page-b/page-b.component').then(m => m.PageBComponent)
+  },
+  {
+    path: 'pageC',
+    loadComponent: () => import('./pages/page-c/page-c.component').then(m => m.PageCComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
